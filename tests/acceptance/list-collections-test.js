72fcb0bd8f54e42249478c94a28fdb5094f6c498
green6erry <green6erry@gmail.com>
2018-04-18 13:48:55 -0400

    set up basic routes

---
+++ tests/acceptance/list-collections-test.js
@@ -0,0 +1,25 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list collections', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('Show message collections on front page', async function (assert) {
+	});
+  test('Link to information about the collection', async function (assert) {
+	});
+  test('Link to user who made each collection', async function (assert) {
+	});
+  test('Filter collections by category, recent, or popular', async function (assert) {
+	});
+  test('Sort collection list by popular', async function (assert) {
+	});
+  test('Show more details about collection', async function (assert) {
+	});
+  test('Show buttons to make a new collection', async function (assert) {
+	});
+  test('Show history of lists users used before', async function (assert) {
+	});
+
+});
