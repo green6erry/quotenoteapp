be918eff5eac398187202e20fef7499c9dac6925
green6erry <green6erry@gmail.com>
2018-04-19 15:53:17 -0400

    toggle image size

--- tests/integration/components/collection-posting-test.js
+++ tests/integration/components/collection-posting-test.js
@@ -6,21 +6,11 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | collection-posting', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    test('should display collection details', async function(assert) {
 
-    await render(hbs`{{collection-posting}}`);
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  test('should toggle wide class on click', async function(assert) {
 
-    // Template block usage:
-    await render(hbs`
-      {{#collection-posting}}
-        template block text
-      {{/collection-posting}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
   });
 });
