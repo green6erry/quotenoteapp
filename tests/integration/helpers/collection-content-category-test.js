ebac9832c4a35c078208370a4977cf9d6f4c0b6c
green6erry <green6erry@gmail.com>
2018-04-19 16:36:39 -0400

    added tests for collections content category helper

--- tests/integration/helpers/collection-content-category-test.js
+++ tests/integration/helpers/collection-content-category-test.js
@@ -6,12 +6,19 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Helper | collection-content-category', function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    this.set('inputValue', '1234');
+  test('it renders correctly for a Topic-based collection', async function(assert) {
+    this.set('inputValue', 'Historical');
 
     await render(hbs`{{collection-content-category inputValue}}`);
 
-    assert.equal(this.element.textContent.trim(), '1234');
+    assert.equal(this.element.textContent.trim(), 'Topic');
+  });
+
+  test('it renders correctly for a custom-made collection', async function(assert) {
+    this.set('inputValue', 'powerful');
+
+    await render(hbs`{{collection-content-category inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), 'Custom');
   });
 });
