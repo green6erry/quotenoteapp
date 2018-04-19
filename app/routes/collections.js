4448731f95f8a5d7702b3f635e64383b3ffc79b1
green6erry <green6erry@gmail.com>
2018-04-19 10:54:51 -0400

    hard-coded models

--- app/routes/collections.js
+++ app/routes/collections.js
@@ -1,4 +1,34 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+model() {
+    return [{
+      id: 'us-presidents',
+      title: 'US Presidents',
+      owner: 'Amanda',
+      city: 'San Francisco',
+      category: 'Historical',
+      quantity: 15,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg/330px-Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg',
+      description: 'The President of the United States is the elected head of state and head of government of the United States. The president leads the executive branch of the federal government and is the commander-in-chief of the United States Armed Forces.'
+    }, {
+      id: 'powerful-women',
+      title: 'Powerful Women',
+      owner: 'Alex Jones',
+      city: 'Seattle',
+      category: 'empowerment',
+      quantity: 13,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Oprah_in_2014.jpg/440px-Oprah_in_2014.jpg',
+      description: 'Hear what some women have said so that you can get through the thing'
+    }, {
+      id: 'funny-writers',
+      title: 'Writers say the Darndest Things',
+      owner: 'Sam Cook',
+      city: 'Portland',
+      category: 'comedy',
+      quantity: 30,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+      description: 'Oscar Wilde was a hoot. Lighten up your day with a collection of ridiculous things writers have said'
+    }];
+  }
 });
